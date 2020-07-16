100e60112680dd07d3e2a4df81b782fd4f693c65
pedro <porso@ucsd.edu>
2020-07-15 19:32:24 -0700

    Finished the canny edge detector; Worked a little with CircleDetection using cv2.HoughCircles

--- Canny/canny.py
+++ Canny/canny.py
@@ -1,8 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("../images/statue_small.jpg", 0)
-cv2.imwrite("canny.jpg", cv2.Canny(img, 200, 300))
-cv2.imshow("canny", cv2.imread("canny.jpg"))
-cv2.waitKey()
-cv2.destroyAllWindows()
+def main():
+    img = cv2.imread("./images/theBall.png", 0)
+    cv2.imwrite("canny.png", cv2.Canny(img, 100, 350))
+    cv2.imshow("canny", cv2.imread("canny.png"))
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
